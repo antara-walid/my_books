47438e0b102fb582eb6c08efc84926ef79a9c2bc
walid <antara.walid.27@gmail.com>
2024-12-07 23:34:21 +0100

    initial commit

---
+++ database.py
@@ -0,0 +1,30 @@
+books = {
+    1: {
+        "name": "The trial",
+        "author": "Franz kafka",
+        "rating": 7.5,
+        "read": True,
+        "tags": ["Philosophy", "psychology"]
+    },
+    2: {
+        "name": "So Good They Can't Ignore you",
+        "author": "Cal Newport",
+        "rating": 8.5,
+        "read": True,
+        "tags": ["Productivity"]
+    },
+    3: {
+        "name": "Notes From the underground",
+        "author": "Fyodor Dostoevsky",
+        "rating": 9,
+        "read": True,
+        "tags": ["Philosophy", "psychology"]
+    },
+    4: {
+        "name": "Everyday Millionaires",
+        "author": "Chris Hogan",
+        "rating": None,
+        "read": False,
+        "tags": ["Money"]
+    }
+}
